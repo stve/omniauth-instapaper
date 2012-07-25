e4e49056f778c79f77ee37a684adeaf698b3ff68
Steve Agalloco <steve.agalloco@gmail.com>
2012-07-25 12:58:02 -0400

    use the proper field descriptions in the OmniAuth form

--- lib/omniauth/strategies/instapaper.rb
+++ lib/omniauth/strategies/instapaper.rb
@@ -19,6 +19,13 @@ module OmniAuth
         {'raw_info' => raw_info }
       end
 
+      def get_credentials
+        OmniAuth::Form.build(options.xauth_options) do
+          text_field 'Email or Username', 'username'
+          password_field 'Password', 'password'
+        end.to_response
+      end
+
       def raw_info
         @raw_info ||= MultiJson.decode(access_token.get("/api/1/account/verify_credentials").body).first
       end
